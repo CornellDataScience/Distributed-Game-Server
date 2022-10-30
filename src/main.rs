0636befa7447e56e1b8ae3d866a9382335c1c1d4
Rafael Chaves <rafaelvchaves@gmail.com>
2022-10-30 14:05:21 -0400

    Add initial RPC implementations
      - Added proto file for RPC service definition
      - Added types for node/log
      - Implemented RequestVote + AppendEntries RPC with initial unit tests
      - Added main function to conditionally launch client or server
        depending on program args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use raft::{raft_client::RaftClient, VoteRequest};
+use tonic::transport::Server;
+pub mod raft {
+    tonic::include_proto!("raft");
+}
+use std::env;
+mod node;
+
+async fn start_rpc_server() -> Result<(), Box<dyn std::error::Error>> {
+    let address = "[::1]:8080";
+    println!("hosting raft RPC server at {}...", address);
+    Server::builder()
+        .add_service(node::new_raft_server(address, Vec::new()))
+        .serve(address.parse().unwrap())
+        .await?;
+    Ok(())
+}
+
+async fn start_client() -> Result<(), Box<dyn std::error::Error>> {
+    let mut follower = RaftClient::connect("http://[::1]:8080").await?;
+    let req = VoteRequest {
+        candidate_id: String::from("abc"),
+        term: 1,
+        last_log_index: None,
+        last_log_term: 0,
+    };
+    println!("sending vote request to follower:");
+    dbg!(req.clone());
+    match follower.request_vote(tonic::Request::new(req)).await {
+        Err(e) => println!("unexpected error: {}", e),
+        Ok(res) => println!("vote granted = {}", res.into_inner().vote_granted),
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("specify 'client' or 'server' role in command-line argument");
+        return ();
+    }
+    return match String::from(&args[1]).as_str() {
+        "client" => assert!(start_client().await.is_ok()),
+        "server" => assert!(start_rpc_server().await.is_ok()),
+        r => println!("unknown role {}", r),
+    };
 }
---
+++ src/node/mod.rs
@@ -0,0 +1,172 @@
+use raft::{
+    raft_server::Raft, raft_server::RaftServer, AppendEntriesRequest, AppendEntriesResponse,
+    LogEntry, VoteRequest, VoteResponse,
+};
+use std::cmp;
+use std::sync::Arc;
+use std::sync::Mutex;
+use tonic::{Request, Response, Status};
+
+#[cfg(test)]
+mod tests;
+
+pub mod raft {
+    tonic::include_proto!("raft");
+}
+
+#[derive(Debug, Default, PartialEq)]
+enum State {
+    #[default]
+    Follower,
+    Candidate,
+    Leader,
+}
+
+#[derive(Debug, Default)]
+pub struct Node {
+    // volatile state
+    id: String,
+    state: State,
+    commit_index: u64,
+    last_applied: u64,
+    peers: Vec<String>,
+    state_machine: std::collections::HashMap<String, i64>,
+
+    // persistent state
+    current_term: u64,
+    voted_for: Option<String>,
+    log: Log,
+
+    // volatile leader state
+    next_index: Vec<u64>,
+    match_index: Vec<u64>,
+}
+
+type Log = Vec<LogEntry>;
+type RaftNode = Arc<Mutex<Node>>;
+
+fn new_node(id: &str, peers: Vec<String>) -> RaftNode {
+    Arc::new(Mutex::new(Node::new(id, peers)))
+}
+
+impl Node {
+    pub fn new(id: &str, peers: Vec<String>) -> Self {
+        Self {
+            id: String::from(id),
+            state: State::Follower,
+            commit_index: 0,
+            last_applied: 0,
+            peers: peers,
+            state_machine: std::collections::HashMap::new(),
+            next_index: Vec::new(),
+            match_index: Vec::new(),
+            current_term: 1,
+            voted_for: None,
+            log: Vec::new(),
+        }
+    }
+
+    fn log_newer_than(&self, log_term: u64, log_index: Option<u64>) -> bool {
+        match log_index {
+            None => self.log.len() > 0,
+            Some(i) => {
+                let last_index = self.log.len() - 1;
+                let last_term = self.log[last_index].term;
+                return last_term > log_term || (last_term == log_term && last_index as u64 >= i);
+            }
+        }
+    }
+
+    fn respond_to_vote(&self, vote_granted: bool) -> Result<Response<VoteResponse>, Status> {
+        Ok(Response::new(VoteResponse {
+            term: self.current_term,
+            vote_granted: vote_granted,
+        }))
+    }
+
+    fn respond_to_ae(&self, success: bool) -> Result<Response<AppendEntriesResponse>, Status> {
+        Ok(Response::new(AppendEntriesResponse {
+            term: self.current_term,
+            success: success,
+        }))
+    }
+}
+
+#[tonic::async_trait]
+impl Raft for RaftNode {
+    async fn request_vote(
+        &self,
+        request: Request<VoteRequest>,
+    ) -> Result<Response<VoteResponse>, Status> {
+        let self = &mut self.lock().unwrap();
+        let req = request.into_inner();
+        if req.term < self.current_term {
+            return self.respond_to_vote(false);
+        }
+        if req.term > self.current_term {
+            self.state = State::Follower;
+            self.current_term = req.term;
+            self.voted_for = None;
+        }
+        let can_vote = match &self.voted_for {
+            None => true,
+            Some(id) => *id == req.candidate_id,
+        };
+        if !can_vote || self.log_newer_than(req.last_log_term, req.last_log_index) {
+            return self.respond_to_vote(false);
+        }
+        self.voted_for = Some(req.candidate_id);
+        return self.respond_to_vote(true);
+    }
+
+    async fn append_entries(
+        &self,
+        request: Request<AppendEntriesRequest>,
+    ) -> Result<Response<AppendEntriesResponse>, Status> {
+        let self = &mut self.lock().unwrap();
+        let req = request.into_inner();
+        if req.term < self.current_term {
+            return self.respond_to_ae(false);
+        }
+        // TODO: reset election timeout here
+        if req.term > self.current_term {
+            self.state = State::Follower;
+            self.current_term = req.term;
+            self.voted_for = Some(req.leader_id);
+        }
+        if req.entries.len() == 0 {
+            // heartbeat: just respond immediately
+            return self.respond_to_ae(true);
+        }
+        let (next_i, is_consistent) = match req.prev_log_index {
+            None => (0, true),
+            Some(i) => {
+                let i = i as usize;
+                (
+                    i + 1,
+                    i < self.log.len() && self.log[i].term == req.prev_log_term,
+                )
+            }
+        };
+        if !is_consistent {
+            return self.respond_to_ae(false);
+        }
+        let n = req.entries.len();
+        for j in 0..n {
+            if next_i + j < self.log.len() {
+                self.log[next_i + j] = req.entries[j].clone();
+                continue;
+            }
+            self.log.push(req.entries[j].clone());
+        }
+        if req.leader_commit > self.commit_index {
+            // not sure if this is correct
+            self.commit_index = cmp::min(req.leader_commit, (self.log.len() - 1) as u64);
+        }
+        return self.respond_to_ae(true);
+    }
+}
+
+pub fn new_raft_server(address: &str, peers: Vec<String>) -> RaftServer<RaftNode> {
+    return RaftServer::new(new_node(address, peers));
+}
