c237805e7f15710cd9dab9aa31a5960b5e184b91
Rafael Chaves <rafaelvchaves@gmail.com>
2022-11-05 17:37:40 -0400

    Add ip address as command line arg

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,17 @@ pub mod raft {
 use std::env;
 mod node;
 
-async fn start_rpc_server() -> Result<(), Box<dyn std::error::Error>> {
-    let address = "[::1]:8080";
-    println!("hosting raft RPC server at {}...", address);
+async fn start_rpc_server(addr: String) -> Result<(), Box<dyn std::error::Error>> {
+    println!("hosting raft RPC server at {}...", addr);
     Server::builder()
-        .add_service(node::new_raft_server(address, Vec::new()))
-        .serve(address.parse().unwrap())
+        .add_service(node::new_raft_server(addr.as_str(), Vec::new()))
+        .serve(addr.parse().unwrap())
         .await?;
     Ok(())
 }
 
-async fn start_client() -> Result<(), Box<dyn std::error::Error>> {
-    let mut follower = RaftClient::connect("http://[::1]:8080").await?;
+async fn connect_to(addr: String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut follower = RaftClient::connect(addr).await?;
     let req = VoteRequest {
         candidate_id: String::from("abc"),
         term: 1,
@@ -36,13 +35,20 @@ async fn start_client() -> Result<(), Box<dyn std::error::Error>> {
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    if args.len() < 3 {
         println!("specify 'client' or 'server' role in command-line argument");
         return ();
     }
+    println!("{}", String::from(&args[2]));
     return match String::from(&args[1]).as_str() {
-        "client" => assert!(start_client().await.is_ok()),
-        "server" => assert!(start_rpc_server().await.is_ok()),
+        "client" => match connect_to(String::from(&args[2])).await {
+            Err(e) => println!("error starting server: {}", e.to_string()),
+            Ok(_) => (),
+        },
+        "server" => match start_rpc_server(String::from(&args[2])).await {
+            Err(e) => println!("error starting server: {}", e.to_string()),
+            Ok(_) => (),
+        },
         r => println!("unknown role {}", r),
     };
 }
