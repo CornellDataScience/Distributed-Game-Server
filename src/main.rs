5dc573fb3f21c4ee698ddc5034873d05b1f3cb21
Iris <icl5@cornell.edu>
2022-11-08 19:42:33 -0500

    making server and client commands nicer to use

--- src/main.rs
+++ src/main.rs
@@ -48,17 +48,33 @@ async fn send_request_vote(
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
+
+    // read from peers.txt to get the ip addresses of server nodes
     let contents = fs::read_to_string("data/peers.txt").expect("cannot read file");
     let peers: Vec<&str> = contents.split(",").collect();
     let n = peers.len();
     if n == 0 {
         println!("please specify peers in data/peers.txt")
     }
+
+    // Summary: starts up a client or server node
+    // How to use commands:
+    // cargo run server <port>
+    // - starts a server listening at specified port (default 8080)
+    // cargo run client
+    // - send message from client to all nodes listed in peers.txt
+
     if args.len() < 2 {
         println!("usage: cargo run -- <client/server>");
         return ();
     }
-    let addr = local_ip().unwrap().to_string() + &String::from(":8080");
+    // run servers from localhost at some port (default 8080)
+    let mut port = String::from(":8080");
+    if args.len() == 3 {
+        port = ":".to_string() + &String::from(&args[2]);
+    }
+
+    let server_addr = local_ip().unwrap().to_string() + &port;
     match String::from(&args[1]).as_str() {
         "client" => {
             let mut votes = 0;
@@ -80,7 +96,7 @@ async fn main() {
             }
             println!("only {} votes received: (need {})", votes, n);
         }
-        "server" => match start_rpc_server(addr).await {
+        "server" => match start_rpc_server(server_addr).await {
             Err(e) => println!("error starting server: {}", e),
             _ => (),
         },
