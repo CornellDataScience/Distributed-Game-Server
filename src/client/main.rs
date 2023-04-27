2f7ce377e12b848eda126baa85d6e11dd1c84e47
Bryant Park <bryantpark04@gmail.com>
2023-04-26 21:41:11 -0400

    Use directory server in client to get peers

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,12 +1,18 @@
 use digs::client::Client;
 use std::fs;
+use std::io::Read;
 #[tokio::main]
 async fn main() {
     // set up a client and make 2 requests, a put then a get
-    let contents = fs::read_to_string("../data/peers.txt").expect("cannot read file");
-    let peers: Vec<String> = contents
-        .split("\n")
-        .map(|addr| String::from("http://") + &String::from(addr))
+    let mut res = reqwest::blocking::get("http://localhost:8000/get-peers")
+        .expect("Could not connect to directory server");
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+    let peers: Vec<String> = serde_json::from_str::<Vec<String>>(&body)
+        .expect("Could not parse JSON response")
+        .into_iter()
+        .filter(|addr| !addr.is_empty())
+        .map(|addr| String::from("http://") + &addr)
         .collect();
     let mut c = Client::new(peers);
     let key = String::from("key");
