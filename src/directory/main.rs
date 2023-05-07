b36e7b9730b5458db24a391924832a1cfd9737eb
Bryant Park <bryantpark04@gmail.com>
2023-05-07 05:25:05 -0400

    Remove old code for reading peers.txt in directory

--- src/directory/main.rs
+++ src/directory/main.rs
@@ -1,7 +1,6 @@
 #[macro_use] extern crate rocket;
 use rocket::State;
 use std::collections::HashSet;
-use std::fs;
 use std::sync::{Arc, Mutex};
 
 // Stole code from https://github.com/SergioBenitez/Rocket/issues/478
@@ -14,12 +13,6 @@ type PeersStatePointer = Arc<Mutex<PeersState>>;
 
 impl PeersState {
     fn new() -> PeersStatePointer {
-        // let contents = fs::read_to_string("data/peers.txt")
-        //     .expect("cannot read file");
-        // let peers: HashSet<String> = contents
-        //     .split("\n")
-        //     .map(|addr| String::from(addr))
-        //     .collect();
         let peers = HashSet::new();
         Arc::new(Mutex::new(PeersState { peers: peers, leader: None }))
     }
